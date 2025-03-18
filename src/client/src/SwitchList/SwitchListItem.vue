5c6d611fa8dd1f3a93e48a81483b1716c1700706
Loc Le <lpl212757@gmail.com>
2025-03-18 19:49:22 +0700

    Update yellow color of button

--- src/client/src/SwitchList/SwitchListItem.vue
+++ src/client/src/SwitchList/SwitchListItem.vue
@@ -47,7 +47,7 @@ const features = ref([
 </script>
 
 <template>
-    <div class="flex flex-col pt-6 pb-4 px-6 items-center justify-between gap-8 border-3 border-black rounded-lg bg-yellow-200 min-h-64">
+    <div class="flex flex-col pt-6 pb-4 px-6 items-center justify-between gap-8 border-3 border-black rounded-lg bg-yellow-50 min-h-64">
         <div class="w-full flex flex-col gap-2">
             <h3 class="text-center text-lg font-libre">{{ props.switchItem.name }}</h3>
             <div class="flex flex-col justify-start w-full text-gray-600">
@@ -56,7 +56,10 @@ const features = ref([
             </div>
         </div>
         <div class="w-full">
-            <div class="cursor-pointer text-center bg-black text-white hover:text-black hover:bg-lime-200 active:bg-lime-300 active:text-black px-4 py-3 rounded-lg w-full">
+            <div class="cursor-pointer text-center bg-black text-white border-2 
+                border-black hover:text-black hover:bg-yellow-200 
+                active:bg-yellow-300 active:text-black 
+                px-4 py-3 rounded-lg w-full">
                 <p>View Details</p>
             </div>
         </div>
--- src/client/src/SwitchList/SwitchListSkeleton.vue
+++ src/client/src/SwitchList/SwitchListSkeleton.vue
@@ -15,7 +15,7 @@ const skeletonFeatures = ref([
     <div class="w-full flex flex-col items-center gap-4">
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-4 w-full">
             <div v-for="index in 12" :key="index"
-                class="flex flex-col pt-6 pb-4 px-6 items-center justify-between gap-8 border-3 border-black rounded-lg bg-yellow-200 min-h-64">
+                class="flex flex-col pt-6 pb-4 px-6 items-center justify-between gap-8 border-3 border-black rounded-lg bg-yellow-50 min-h-64">
                 <div class="w-full flex flex-col gap-2">
                     <div class="h-6 bg-gray-300 rounded w-1/2 mx-auto animate-pulse"></div>
                     <div class="flex flex-col justify-start w-full text-gray-600">
