146c87016d88d0bfa7fd7b7df71e5933076d33a7
Loc Le <lpl212757@gmail.com>
2025-03-23 12:24:58 +0700

    remove console log

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
-    console.log(env);
 
     return {
         plugins: [vue(), tailwindcss()],
